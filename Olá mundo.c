0313018279a6a2bd2a93b5e455bbcf6ca4aadb96
Ana Karoliny Veras <anakaroliny5555@gmail.com>
2025-02-07 13:11:31 +0000

    CRIANDO IF

--- Olá mundo.c
+++ Olá mundo.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+int main (){
+ 
+   int numero = 5, resultado;
 
+   resultado = numero % 2;
+     printf("variável resultado é: %d\n", resultado);
+
+   if(resultado == 0){
+     printf(" o número é par.\n");
+
+}
+}
