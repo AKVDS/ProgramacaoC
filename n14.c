fb8704590b6a853ba8ac395d98346628b7eb7efc
Ana Karoliny Veras <anakaroliny5555@gmail.com>
2025-02-20 12:18:04 +0000

    : usando o oposto exemplo 2

---
+++ n14.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+
+    int temperatura = 30;
+    int resulado;
+
+    temperatura > 31 ? printf ("Está calor. \n") : printf ("Está frio. \n");
+
+    return 0;
+
+}
