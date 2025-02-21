75be97d0df09f2baaad0f9f371bba4439cc66d62
Ana Karoliny Veras <anakaroliny5555@gmail.com>
2025-02-21 11:39:19 +0000

    WHILE (LOOP)

---
+++ n17.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+    int i = 1;
+
+    while (i <= 5)
+    {
+        printf ("%d\n", i);
+        i++; // ++ vai pegar o valor de i e vai somar até chegar no resultado que foi pedido
+    }
+return 0;
+}
+
