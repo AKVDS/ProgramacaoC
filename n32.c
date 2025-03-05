37e0eb64583a555cf42727994f10fdd38f68a551
Ana Karoliny Veras <anakaroliny5555@gmail.com>
2025-03-05 00:43:05 +0000

    busca condicional - matriz

--- n32.c
+++ n32.c
@@ -2,23 +2,23 @@
 int main () {
 
     int matriz[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int elementoEncontrado = 5;
-    int elementoNaoencon = 0;  // Variável que indicará se o elemento foi encontrado
+    int Alvo = 5;
+    int encontrado = 0;  // Variável que indicará se o elemento foi encontrado
 
     // Busca condicional do elemento alvo
     for (int a = 0; a < 3; a++) {  // Loop externo - linhas
         for (int b = 0; b < 3; b++) {  // Loop interno - colunas
-            if (matriz[a][b] == elementoEncontrado) {
-                printf("Elemento %d encontrado na posição (%d, %d)\n", elementoEncontrado, a, b);
-                elementoNaoencon = 1;  // Atualiza a variável para indicar que o elemento foi encontrado
+            if (matriz[a][b] == Alvo) {
+                printf("Elemento %d encontrado na posição (%d, %d)\n", Alvo, a, b);
+                encontrado = 1;  // Atualiza a variável para indicar que o elemento foi encontrado
                 break;
             }
         }
-        if (elementoNaoencon) break;  // Se o elemento foi encontrado, sai do loop externo
+        if (encontrado) break;  // Se o elemento foi encontrado, sai do loop externo
     }
 
-    if (!elementoNaoencon) {  // Se o elemento não foi encontrado
-        printf("Elemento %d não encontrado na matriz.\n", elementoEncontrado);
+    if (!encontrado) {  // Se o elemento não foi encontrado
+        printf("Elemento %d não encontrado na matriz.\n", Alvo);
     }
 
     return 0;
